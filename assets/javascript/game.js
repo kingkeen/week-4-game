fed2c40af14dbe3e69ae0c3d178baf2222979146
Joaquin Ignacio <Keen@Revolt.local>
2017-05-17 18:18:57 -0700

    running game finally

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,12 +2,15 @@
 // GLOBAL VARIABLES: 
 // - WINS
 // - LOSES
-// - CRYSTAL VALUES ARRAY
 
 var playerWins = 0;
 var playerLoses = 0;
-
-
+var targetNumber;
+var currentScore;
+var button1;
+var button2;
+var button3;
+var button4;
 
 
 // COMPLETE ROUND FUNCTION () (CONSIDER NAMING INITIALIZE-GAME OR SIMILAR)
@@ -15,62 +18,124 @@ var playerLoses = 0;
 function initialGame (){
 
 	// IN GAME VARIABLES:
-	var targetNumber;
-	var currentScore;
-	var button1 = Math.floor((Math.random()*12) +1);
-	var button2 = Math.floor((Math.random()*12) +1);
-	var button3 = Math.floor((Math.random()*12) +1);
-	var button4 = Math.floor((Math.random()*12) +1);
+	currentScore = 0;
+	$("#currentScore").html(currentScore);
+	// declares variables for the buttons and assigns them random number between 1-12
+	button1 = Math.floor((Math.random()*12) +1);
+	button2 = Math.floor((Math.random()*12) +1);
+	button3 = Math.floor((Math.random()*12) +1);
+	button4 = Math.floor((Math.random()*12) +1);
 
-	console.log("Button 1: ", button1)
+	console.log("Button 1: ", button1);
+	console.log("Button 2: ", button2);
+	console.log("Button 3: ", button3);
+	console.log("Button 4: ", button4);
 
-	console.log("Button 2: ", button2)
-
-	console.log("Button 3: ", button3)
-
-	console.log("Button 4: ", button4)
-
-	// ARRAY OF CRYSTAL-VARIABLES (BUTTON#) [CRYSTAL 0, CRYSTAL 1, CRYSTAL 2, CRYSTAL 3]
-	// VAR TARGETPOINTS = 0
-	// VAR CURRENT-TOTAL OF POINTS = 0
 
 	// GENERATE RANDOM NUMBER FOR THE TARGETPOINTS BETWEEN 19-120;
 	function createGoal () {
 		var min =  19;
 		var max = 120;
 		targetNumber = Math.floor(Math.random()*(max-min+1) +min);
-		return targetNumber;
+		console.log("Target Number: ", targetNumber);
+		$("#targetNumber").html(targetNumber);
 	}
+
 	createGoal()
-			console.log("Target Number: ", targetNumber);
+
+	// the below code is not pushing to the HTML. 
 
 
-	// 		use jQuery #each method to access each crystal, then randomly assign a numerical value!
-
-
-
-
-}
+};
  
- $('button1').on("click", function() {
+
+
+$(document).ready(function(){
+	
+	$("#button1").click(function(){
  	currentScore += button1;
- 	document.querySelector("#currentScore").innerHTML = currentScore;
+
  	console.log("Score is ", currentScore);
+ 	$("#currentScore").html(currentScore);
 
  	if (currentScore ===  targetNumber) {
  		playerWins++;
- 		document.querySelector("#playerWin").innerHTML = playerWins;
+ 		$("#playerWin").html(playerWins);
  		initialGame();
- 	};
+ 	}
 
- 	if (currentScore >= targetNumber) {
+ 	else if (currentScore >= targetNumber) {
  		playerLoses++;
- 		document.querySelector("#playerLose").innerHTML = playerLoses;
+ 		$("#playerLose").html(playerLoses);
  		initialGame();
- 	};
+ 	}
+ });
+
+	
+	$("#button2").click(function(){
+ 	currentScore += button2;
+
+ 	console.log("Score is ", currentScore);
+ 	$("#currentScore").html(currentScore);
+
+ 	if (currentScore ===  targetNumber) {
+ 		playerWins++;
+ 		$("#playerWin").html(playerWins);
+ 		initialGame();
+ 	}
+
+ 	else if (currentScore >= targetNumber) {
+ 		playerLoses++;
+ 		$("#playerLose").html(playerLoses);
+ 		initialGame();
+ 	}
+});
+
+	
+	$("#button3").click(function(){
+ 	currentScore += button3;
+
+ 	console.log("Score is ", currentScore);
+ 	$("#currentScore").html(currentScore);
+
+ 	if (currentScore ===  targetNumber) {
+ 		playerWins++;
+ 		$("#playerWin").html(playerWins);
+ 		initialGame();
+ 	}
+
+ 	else if (currentScore >= targetNumber) {
+ 		playerLoses++;
+ 		$("#playerLose").html(playerLoses);
+ 		initialGame();
+ 	}
+});
+
+	
+	$("#button4").click(function(){
+ 	currentScore += button4;
+
+ 	console.log("Score is ", currentScore);
+ 	$("#currentScore").html(currentScore);
+
+ 	if (currentScore ===  targetNumber) {
+ 		playerWins++;
+ 		$("#playerWin").html(playerWins);
+ 		initialGame();
+ 	}
+
+ 	else if (currentScore >= targetNumber) {
+ 		playerLoses++;
+ 		$("#playerLose").html(playerLoses);
+ 		initialGame();
+ 	}
+
  });
 
  initialGame ();
+
+});
+ 
 
 
 // PROCESS TO RUN THE ACTUAL GAME: 
